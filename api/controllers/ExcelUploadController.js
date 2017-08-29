331a63094a7b99dd985dc80c75b12bf59453f1f4
kothavaleganesh <ganesh.wohlig@gmail.com>
2017-08-29 12:36:11 +0530

    ExcelUpload test

--- api/controllers/ExcelUploadController.js
+++ api/controllers/ExcelUploadController.js
@@ -662,93 +662,6 @@ var controller = {
         }
     },
 
-    // finalUploadForCustomerNote: function (req, res) {
-    //     Config.importGSForCustomFields(req.body.name, req.body.fields, function (err, data) {
-    //         if (err || _.isEmpty(data)) {
-    //             res.callback(err);
-    //         } else {
-    //             async.concatSeries(data, function (singleData, callback) {
-    //                 async.waterfall([
-    //                         function (callback) {
-    //                             var finalData = {};
-    //                             var totalCount = 0;
-    //                             var sucessCount = 0;
-    //                             var FailureCount = 0;
-    //                             CustomerNote.saveData(singleData, function (err, found) {
-    //                                 if (err) {
-    //                                     console.log('********** error at 1st function of asynch.waterfall in search of ProjectExpense.js ************', err);
-    //                                     callback(null, err);
-    //                                     FailureCount++;
-    //                                 } else {
-    //                                     if (_.isEmpty(found)) {
-    //                                         callback(null, err);
-    //                                     } else {
-    //                                         console.log("found!!!!", found);
-    //                                         sucessCount++;
-    //                                         finalData.sucessCount = sucessCount;
-    //                                         finalData.totalCount = FailureCount + sucessCount;
-    //                                         finalData.failureCount = FailureCount;
-    //                                         finalData.found = found;
-    //                                         console.log("finalDaata----", finalData);
-    //                                         callback(null, finalData);
-    //                                     }
-    //                                 }
-    //                             });
-    //                         },
-    //                         function (finalData, callback) {
-    //                             console.log("data---finalData-----finalData", finalData);
-    //                             var dataFinal = {};
-    //                             var eData = {};
-    //                             eData.tableName = 'CustomerNote';
-    //                             eData.logs = finalData.found;
-    //                             AllLogs.saveData(eData, function (err, found) {
-    //                                 if (err) {
-    //                                     console.log('********** error at 1st function of asynch.waterfall in search of ProjectExpense.js ************', err);
-    //                                     callback(err, null);
-    //                                 } else {
-    //                                     if (_.isEmpty(found)) {
-    //                                         callback(err, null);
-    //                                     } else {
-    //                                         delete finalData.found;
-
-    //                                         console.log("dataFinal----", finalData);
-
-    //                                         callback(null, finalData);
-    //                                     }
-    //                                 }
-    //                             });
-    //                         }
-    //                     ],
-    //                     function (err, found) {
-    //                         if (err) {
-    //                             console.log('********** error at final response of asynch.waterfall in search of ProjectExpense.js ************', err);
-    //                             callback(err, null);
-    //                         } else {
-    //                             if (_.isEmpty(found)) {
-    //                                 callback(err, null);
-    //                             } else {
-    //                                 callback(null, found);
-    //                             }
-    //                         }
-    //                     });
-    //                 // }
-    //             }, function (err, data) {
-    //                 if (err) {
-    //                     console.log('********** error at final response of asynch.waterfall in search of ProjectExpense.js ************', err);
-    //                     callback(err, null);
-    //                 } else {
-    //                     if (_.isEmpty(found)) {
-    //                         callback(err, null);
-    //                     } else {
-    //                         callback(null, found);
-    //                     }
-    //                 }
-    //             });
-    //         }
-    //     });
-    // },
-
-
     finalUploadForCustomerNote: function (req, res) {
         Config.importGSForCustomFields(req.body.name, req.body.fields, function (err, data) {
             if (err || _.isEmpty(data)) {
@@ -804,10 +717,10 @@ var controller = {
                                         callback(err, null);
                                     } else {
                                         console.log("dataFinal----", data);
-                                        res.callback(null, data);
                                     }
                                 }
                             });
+                            res.callback(null, data[data.length - 1]);
                         }
                     }
                 });
