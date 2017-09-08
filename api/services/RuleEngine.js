e4ed0be688b3d689f51ad08132fe0daab6c31d6d
aditya <aditya.ghag@wohlig.com>
2017-09-08 21:40:02 +0530

    chnages

--- api/services/RuleEngine.js
+++ api/services/RuleEngine.js
@@ -117,13 +117,15 @@ var model = {
     getValueFromRuleEngine: function (tableName, tableField, transactionJson) {
         var returnVal = "";
         var returnTable = "";
+        // red(tableName);
+        // green(tableField);
         if (tableName == "Company") {
             returnTable = transactionJson.companyData;
         } else if (tableName == "Item") {
             returnTable = transactionJson.itemData;
         } else if (tableName == "WarrantyItem") {
             returnTable = transactionJson.itemData.warrantyItemId;
-        } else if (tableName == "Location") {
+        } else if (tableName == "Locations") {
             returnTable = transactionJson.locationData;
         } else if (tableName == "TillRegister") {
             returnTable = transactionJson.tillRegisterData;
@@ -143,73 +145,166 @@ var model = {
 
     findViolationCount: function (ruleId, callback) {
         // callback with count or error
+        // Transation.count({violation:ruleId}).exec(callback);
     },
 
-    getVoilationData: function (data, callback) {
-        Transaction.find({}, function (err, found) {
-            if (err || _.isEmpty(found)) {
-                console.log(err);
-                callback(err, null);
-            } else if (found) {
-                async.concatLimit(found, 20, function (transactionData, callback) {
-                    Transaction.clearAllPreviousViolations(transactionData._id, callback);
-                    RuleEngine.find({}, function (err, found1) {
-                        if (err || _.isEmpty(found1)) {
-                            console.log(err);
-                            callback(err, null);
-                        } else if (found1) {
-                            async.concatSeries(found1, function (rule, callback) {
-                                async.concatSeries(rule.rule, function (rulesData, callback) {
-                                    var value1 = RuleEngine.getValueFromRuleEngine(rulesData.model, rulesData.field, transactionData.transactionJson);
-                                    if (!_.isEmpty(rulesData.constant)) {
-                                        value2 = rulesData.constant;
-                                    } else {
-                                        value2 = RuleEngine.getValueFromRuleEngine(rulesData.table, rulesData.tableField, transactionData.transactionJson);
-                                    }
-                                    if (rulesData.operators == '==') {
-                                        conditionData = value1 == value2;
-                                    } else if (rulesData.operators == '<=') {
-                                        conditionData = parseFloat(value1) <= parseFloat(value2);
-                                    } else if (rulesData.operators == '>=') {
-                                        conditionData = parseFloat(value1) >= parseFloat(value2);
-                                    } else if (rulesData.operators == '>') {
-                                        conditionData = parseFloat(value1) > parseFloat(value2);
-                                    } else if (rulesData.operators == '<') {
-                                        conditionData = parseFloat(value1) < parseFloat(value2);
-                                    } else if (rulesData.operators == '!=') {
-                                        conditionData = value1 != value2;
-                                    }
-                                    var rules = [{
-                                        name: rulesData.model,
-                                        "condition": function (R) {
-                                            R.when(conditionData);
-                                        },
-                                        "consequence": function (R) {
-                                            R.next();
-                                        }
-                                    }];
-                                    var R = new RuleEng(rules);
-                                    R.execute(transactionData, function (result) {
-                                        if (!_.isEmpty(result.matchPath)) {
-                                            Transaction.addViolation(transactionData._id, rule._id, callback);
-                                        } else {
-                                            //callback(null, "Error");
-                                        }
-                                    });
-                                }, function (err, data) {
-                                    callback(null, data);
-                                });
-                            }, function (err, result) {
-                                //callback(null, result);
-                            });
-                        }
-                    });
-                }, function (err, resultArr) {
-                    callback(null, resultArr);
-                });
-            }
+    // getVoilationData: function (data, callback) {
+    //     Transaction.find({}, function (err, found) {
+    //         if (err || _.isEmpty(found)) {
+    //             console.log(err);
+    //             callback(err, null);
+    //         } else if (found) {
+    //             async.concatLimit(found, 20, function (transactionData, callback) {
+    //                 Transaction.clearAllPreviousViolations(transactionData._id, function () {});
+    //                 RuleEngine.find({}, function (err, found1) {
+    //                     if (err || _.isEmpty(found1)) {
+    //                         console.log(err);
+    //                         callback(err, null);
+    //                     } else if (found1) {
+    //                         async.concatSeries(found1, function (rule, callback) {
+    //                             async.concatSeries(rule.rule, function (rulesData, callback) {
+    //                                 var value1 = RuleEngine.getValueFromRuleEngine(rulesData.model, rulesData.field, transactionData.transactionJson);
+    //                                 var value2;
+    //                                 if (!_.isEmpty(rulesData.constant)) {
+    //                                     value2 = rulesData.constant;
+    //                                 } else {
+    //                                     value2 = RuleEngine.getValueFromRuleEngine(rulesData.table, rulesData.tableField, transactionData.transactionJson);
+    //                                 }
+    //                                 console.log(value1);
+    //                                 console.log(value2);
+    //                                 if (rulesData.operators == '==') {
+    //                                     conditionData = value1 == value2;
+    //                                 } else if (rulesData.operators == '<=') {
+    //                                     conditionData = parseFloat(value1) <= parseFloat(value2);
+    //                                 } else if (rulesData.operators == '>=') {
+    //                                     conditionData = parseFloat(value1) >= parseFloat(value2);
+    //                                 } else if (rulesData.operators == '>') {
+    //                                     conditionData = parseFloat(value1) > parseFloat(value2);
+    //                                 } else if (rulesData.operators == '<') {
+    //                                     conditionData = parseFloat(value1) < parseFloat(value2);
+    //                                 } else if (rulesData.operators == '!=') {
+    //                                     conditionData = value1 != value2;
+    //                                 }
+    //                                 var rules = [{
+    //                                     name: rulesData.model,
+    //                                     "condition": function (R) {
+    //                                         R.when(conditionData);
+    //                                     },
+    //                                     "consequence": function (R) {
+    //                                         R.next();
+    //                                     }
+    //                                 }];
+    //                                 var R = new RuleEng(rules);
+    //                                 R.execute(transactionData, function (result) {
+    //                                     if (!_.isEmpty(result.matchPath)) {
+    //                                         Transaction.addViolation(transactionData._id, rule._id, callback);
+    //                                     } else {
+    //                                         callback(null, "No Violation");
+    //                                     }
+    //                                 });
+    //                             }, function (err, data) {
+    //                                 callback(null, data);
+    //                             });
+    //                         }, function (err, result) {
+    //                             callback(null, result);
+    //                         });
+    //                     }
+    //                 });
+    //             }, callback);
+    //         }
+    //     });
+    // },
+    checkViolationForRule: function (ruleId, callback) {
+        // get count of all transactions get total_pages = totalcount/20;
+        // i = 0 
+        // async.timesSeries(total_pages, iteratee, callback)
+        //      itetaty function async each series = > find skip = ((i)*20)  limit = 20 transactions i++;
+        Transaction.find({}, function (err, transactions) {
+            async.concatLimit(transactions, 20, function (transaction, callback) {
+                RuleEngine.checkViolation(transaction._id, ruleId, callback);
+            }, callback);
         });
     },
+    checkViolationForTransaction: function (transactionId, callback) {
+
+        Transaction.clearAllPreviousViolations(transactionId, function () {});
+        RuleEngine.find({}, function (err, rules) {
+            async.concatLimit(rules, 20, function (rule, callback) {
+                RuleEngine.checkViolation(transactionId, rule._id, callback);
+            }, callback);
+        });
+    },
+    /**
+     * This function check violation of particular rule with particular Transaction.
+     * @param {ObjectId} transactionId transaction id should be provided
+     * @param {ObjectId} ruleId rule id should be provided
+     * @param {function} callback rule id should be provided
+     */
+    checkViolation: function (transactionId, ruleId, callback) {
+        async.waterfall([
+            function (callback) { // get Transaction
+                Transaction.findOne({
+                    _id: transactionId
+                }).exec(callback);
+            },
+            function (transaction, callback) { // get Rule
+                RuleEngine.findOne({
+                    _id: ruleId
+                }).exec(function (err, rule) {
+                    callback(err, transaction, rule);
+                });
+            }
+        ], function (err, transactionData, rulesData) { // execute Violation
+            if (err || _.isEmpty(transactionData) || _.isEmpty(rulesData)) {
+                callback(null, "Error");
+            } else {
+                async.concatSeries(rule.rule, function (rulesData, callback) {
+                    var value1 = RuleEngine.getValueFromRuleEngine(rulesData.model, rulesData.field, transactionData.transactionJson);
+                    var value2;
+                    if (!_.isEmpty(rulesData.constant)) {
+                        value2 = rulesData.constant;
+                    } else {
+                        value2 = RuleEngine.getValueFromRuleEngine(rulesData.table, rulesData.tableField, transactionData.transactionJson);
+                    }
+                    console.log(value1);
+                    console.log(value2);
+                    if (rulesData.operators == '==') {
+                        conditionData = value1 == value2;
+                    } else if (rulesData.operators == '<=') {
+                        conditionData = parseFloat(value1) <= parseFloat(value2);
+                    } else if (rulesData.operators == '>=') {
+                        conditionData = parseFloat(value1) >= parseFloat(value2);
+                    } else if (rulesData.operators == '>') {
+                        conditionData = parseFloat(value1) > parseFloat(value2);
+                    } else if (rulesData.operators == '<') {
+                        conditionData = parseFloat(value1) < parseFloat(value2);
+                    } else if (rulesData.operators == '!=') {
+                        conditionData = value1 != value2;
+                    }
+                    var rules = [{
+                        name: rulesData.model,
+                        "condition": function (R) {
+                            R.when(conditionData);
+                        },
+                        "consequence": function (R) {
+                            R.next();
+                        }
+                    }];
+                    var R = new RuleEng(rules);
+                    R.execute(transactionData, function (result) {
+                        if (!_.isEmpty(result.matchPath)) {
+                            Transaction.addViolation(transactionData._id, rule._id, callback);
+                        } else {
+                            callback(null, "No Violation");
+                        }
+                    });
+                }, function (err, data) {
+                    callback(null, data);
+                });
+            }
+
+        });
+    }
 
 };
 module.exports = _.assign(module.exports, exports, model);
