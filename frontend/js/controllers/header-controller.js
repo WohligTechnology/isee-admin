519958ef01324c7f49358ce61c080a7670732a63
Mahesh Nadar <mahesh.nadar@wohlig.com>
2017-09-16 19:19:06 +0530

    sidemenu with icon

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -7,22 +7,41 @@ myApp.controller('headerCtrl', function ($scope, TemplateService, $state, $scope
 
   //submenu
   $scope.oneAtATime = true;
+
   $scope.classNg = "open";
+
+  $scope.openiconmenu = function () {
+    $scope.classNg = "menucollapse";
+    $scope.sideMenu();
+  }
   $scope.sideMenu = function () {
     console.log("menu");
-
+    if (!$('#sidemenu').hasClass('menucollapse')) {
+      console.log("inside menucollapse");
+      $scope.classNg = "open";
+    }
 
     if ($scope.classNg === "open") {
-      $scope.classNg = "collapsed";
-      $('#sidemenu').addClass("collapsed");
-      // $scope.classNg = "menucollapse";    //for animated side menu
-      // $('#sidemenu').addClass("menucollapse");
+      // $scope.classNg = "collapsed";
+      // $('#sidemenu').addClass("collapsed");
+      $scope.classNg = "menucollapse"; //for animated side menu
+      $('#sidemenu').addClass("menucollapse");
+      $('#iseelogo').css("width", "70px");
       $('#header-collapsed-icon').css("display", "block"); //to make icon visible in header
+      $('.accordion-custom').css("display", "none");
+      $('.iseetitle').css("opacity", "0");
+      $('.sideicon').css("opacity", "1");
+      // console.log("classNg", $scope.classNg);
     } else {
       $scope.classNg = "open";
-      $('#sidemenu').removeClass("collapsed");
-      //  $('#sidemenu').removeClass("menucollapse");
+      // $('#sidemenu').removeClass("collapsed");
+      $('#sidemenu').removeClass("menucollapse");
+      $('#iseelogo').css("width", "100px");
       $('#header-collapsed-icon').css("display", "none"); //to hide icon in header 
+      $('.accordion-custom').css("display", "block");
+      $('.iseetitle').css("opacity", "1");
+      $('.sideicon').css("opacity", "0");
+      // console.log("classNg", $scope.classNg);
     }
   };
 
