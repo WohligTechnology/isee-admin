83b6add72365b7b43e872ea4bdf945e96577dae3
Chintan <chintan@wohlig.com>
2017-09-19 11:21:13 +0530

    changes for nossl

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 8081,
-  realHost: "https://isee.frontidainc.com",
+  realHost: "http://isee.frontidainc.com",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
