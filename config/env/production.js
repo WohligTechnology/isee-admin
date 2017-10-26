f3e42b5a259ac88bf6e848afc06613800494d236
siddTalekar <sidd.tale@gmail.com>
2017-10-26 15:34:53 +0530

    changes

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 8080,
-  realHost: "http://isee.frontidainc.com:8080",
+  realHost: "http://isee.frontidainc.com",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
